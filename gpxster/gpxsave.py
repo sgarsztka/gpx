319ddc5bf33176978771eabbfc016d5743f558d4
sgarsztka <sbl86al.o@gmail.com>
2020-09-26 00:31:54 +0200

    simple gpx parser added

--- gpxster/gpxsave.py
+++ gpxster/gpxsave.py
@@ -1,6 +1,7 @@
 import time
 import os
 from django.conf import settings
+from xml.dom.minidom import parse, parseString
 
 def handle_uploaded_file(f):
     timestr = time.strftime("%Y%m%d-%H%M%S")
@@ -10,3 +11,16 @@ def handle_uploaded_file(f):
     with open(filename, 'wb+') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
+
+    dom1 = parse(filename)
+
+
+    points = dom1.getElementsByTagName("trkpt")
+    for point in points:
+        lat = point.getAttribute("lat")
+        lon = point.getAttribute("lon")
+        elevation = point.getElementsByTagName("ele")[0]
+        times = point.getElementsByTagName("time")[0]
+        print("lat:%s, lon:%s , ele:%s, time:%s" %
+              (lat, lon, elevation.firstChild.data, times.firstChild.data))
+    os.remove(filename)
